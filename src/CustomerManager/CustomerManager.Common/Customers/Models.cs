825e4e3df3b297a2b50ab5b1782b1786c00009df
ferrad.fm@gmail.com <ferrad.fm@gmail.com>
2018-04-25 17:42:57 +0200

    commit 17:42

---
+++ src/CustomerManager/CustomerManager.Common/Customers/Models.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CustomerManager.Common.Data.Models
+{
+    public class CustomerTypeModel
+    {
+        public CustomerTypeModel(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Guid Id { get; }
+        public string Name { get; }
+    }
+}
---
+++ src/CustomerManager/CustomerManager.DataAccess/Customers/Repositories.cs
@@ -0,0 +1,31 @@
+﻿using CustomerManager.Common.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Insight.Database;
+
+namespace CustomerManager.DataAccess
+{
+    public class CustomerTypeRepository
+    {
+        private readonly string _connectionString;
+
+        public CustomerTypeRepository(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public IEnumerable<CustomerTypeModel> GetAll()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var customers = connection.Query<CustomerTypeModel>("dbo.RetrieveCustomerTypes");
+
+                return customers;
+            }
+        }
+    }
+}
